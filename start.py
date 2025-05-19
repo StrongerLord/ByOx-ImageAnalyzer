3c900974937419201e3fde58cb865b34af66b53b
Jesús Adán Salazar Campos <adan10104334@gmail.com>
2025-05-18 22:49:32 -0600

    feat: new endpoint for consulting backend status

--- start.py
+++ start.py
@@ -45,5 +45,10 @@ def analyze():
 
 	return jsonify({'result': result, 'image': img_as_text}), 200
 
+@app.route('/', methods=['GET'])
+def main():
+	return jsonify({'OK', 'All good'})
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
